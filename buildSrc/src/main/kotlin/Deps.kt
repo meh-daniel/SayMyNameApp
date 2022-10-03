af82ece4a513cdb125ac8307d5e957eac87c96c1
Danil Galkin <mehanikdanil@gmail.com>
2022-10-03 12:08:11 +0700

    readme

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -12,10 +12,6 @@ object Deps {
         const val RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:${lifecycle}"
     }
 
-    object Javax {
-        const val INJECT = "javax.inject:javax.inject:1"
-    }
-
     object Room {
         private const val room = "2.3.0"
         const val KTX = "androidx.room:room-ktx:${room}"
